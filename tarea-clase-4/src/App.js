f35fd8c20e906371dedd0b967c89873b70e6818a
Nicolas-85 <nicolasmartinalvarez85@gmail.com>
2022-04-25 23:39:16 -0300

    Initial commit

---
+++ tarea-clase-4/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+//Components
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ tarea-clase-4/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+function Login() {
+    return(
+        <main className='containerTotalLogin'>
+            <div className='containerTitle'>
+                <h1>Stax Food Admin Login</h1>
+            </div>
+            <div className='containerBoxLogin'>
+
+            </div>
+        </main>
+    )
+}
+ 
+export default Login;
